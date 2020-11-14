08643fd8a4a401fee8d70f2c232145b1a4efb6bd
Miia Nyyssönen <mi.nyyssonen@gmail.com>
2020-11-14 21:46:12 +0200

    variables added in js and first eventlisteners added

--- rock_paper.js
+++ rock_paper.js
@@ -1,13 +1,46 @@
 let startBtn = document.getElementById('start-button');
 let mainContentContainer = document.getElementById('main-content');
 
+//Paper, rock, scissors icons buttons
+let paperIconButton = document.getElementById('paper-icon-button');
+let rockIconButton = document.getElementById('rock-icon-button');
+let scissorsIconButton = document.getElementById('scissors-icon-button');
 
-//When button is pressed the button itself disapears and main content comes visible
-startBtn.onclick = function() {
+//The box where the players choice is shown 
+let paperPlayersChoice = document.getElementById('players-choice-paper');
+let rockPlayersChoice = document.getElementById('players-choice-rock');
+let scissorsPlayerChoice = document.getElementById('players-choice-scissors');
+
+
+//When start button is pressed the button itself disapears and main content comes visible
+function startButtonIsPressed() {
     startBtn.style.display = "none";
     mainContentContainer.style.visibility = "visible";
-    }
+    rockPlayersChoice.style.display = "block";
+}
 
 
+function paperButtonIsPressed () {
+    paperPlayersChoice.style.display = "block";
+    rockPlayersChoice.style.display = "none";
+    scissorsPlayerChoice.style.display = "none";
+}
 
+function rockButtonIsPressed() {
+    rockPlayersChoice.style.display = "block";
+    paperPlayersChoice.style.display = "none";    
+    scissorsPlayerChoice.style.display = "none"
+}
+
+function scissorsButtonIsPressed() {
+    scissorsPlayerChoice.style.display = "block";
+    rockPlayersChoice.style.display = "none";
+    paperPlayersChoice.style.display = "none"; 
+}
+
+startBtn.addEventListener("click", startButtonIsPressed);
+//Event Listeners when icon buttons are pressed
+paperIconButton.addEventListener("click", paperButtonIsPressed);
+rockIconButton.addEventListener("click", rockButtonIsPressed);
+scissorsIconButton.addEventListener("click", scissorsButtonIsPressed);
 
